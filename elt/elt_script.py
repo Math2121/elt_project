280417401a9daee58acb439ff89b167408b099f0
Matheus da Silva de Paula <matheus.silva@startyourapp.com.br>
2024-04-18 14:03:14 -0300

    refactor: clean the code and remove wrong command

--- elt/elt_script.py
+++ elt/elt_script.py
@@ -5,18 +5,20 @@ def wait_for_postgres(host, max_retries=5, delay_seconds=5):
     retries = 0
     while retries < max_retries:
         try:
-            result = subprocess.run(["pg_isready","-h", host], check=True, capture_output=True, text=True)
+            result = subprocess.run(
+                ["pg_isready", "-h", host], check=True, capture_output=True, text=True)
             if "accepting connections" in result.stdout:
-                print("Postgres is ready!")
+                print("Successfully connected to PostgreSQL!")
                 return True
         except subprocess.CalledProcessError as e:
-                print("Postgres is not ready yet... {e}")
-                retries += 1
-                print("Retrying in {delay_seconds} seconds...(Attempt {retries}/{max_retries} )")
-                time.sleep(delay_seconds)
-    print("Max retries reached. Exiting...")
+            print(f"Error connecting to PostgreSQL: {e}")
+            retries += 1
+            print(
+                f"Retrying in {delay_seconds} seconds... (Attempt {retries}/{max_retries})")
+            time.sleep(delay_seconds)
+    print("Max retries reached. Exiting.")
     return False
-    
+
 if not wait_for_postgres(host="source_postgres"):
     exit(1)
 
@@ -24,34 +26,32 @@ print("Start ELT script")
 
 source_config = {
     'dbname': 'source_db',
-    'user': 'root',
+    'user': 'postgres',
     'password': 'root',
     'host': 'source_postgres',
 }
 
 destination_config = {
     'dbname': 'destination_db',
-    'user': 'root',
+    'user': 'postgres',
     'password': 'root',
     'host': 'destination_postgres',
 }
 
 
 dump_command = [
-    "pg_dump",
+    'pg_dump',
     '-h', source_config['host'],
     '-U', source_config['user'],
     '-d', source_config['dbname'],
     '-f', 'data_dump.sql',
     '-w'
 ]
-
 subprocess_env = dict(PGPASSWORD=source_config['password'])
 
 subprocess.run(dump_command, env=subprocess_env, check=True)
-
 load_command = [
-    'pgsql',
+    'psql',
     '-h', destination_config['host'],
     '-U', destination_config['user'],
     '-d', destination_config['dbname'],
